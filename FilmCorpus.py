0e5eab0521b1a6a087e002a40fbcd74ae13fef4e
snakeztc <zhaotiancheng.hz@gmail.com>
2016-10-19 00:55:49 -0400

    clean data finish

--- FilmCorpus.py
+++ FilmCorpus.py
@@ -1,47 +1,109 @@
 import os
 import fnmatch
 import re
+import numpy as np
+import nltk
+
 
 class FilmCorpus(object):
+    dump_name = "clean_data.txt"
+
     def __init__(self, corpus_path):
         """
         :param corpus_path: the folder that contains the data
         """
         self._path = corpus_path
 
-        film_files = []
-        for root, _, file_names in os.walk(self._path):
-            for filename in fnmatch.filter(file_names, '*.txt'):
-                film_files.append(os.path.join(root, filename))
-        print("Loaded %d films" % len(film_files))
+        self.dialogs = self.load_dialogs()
 
-        dialogs = {}
-        utt_cnt = 0
-        word_cnt = 0
-        for f_idx, file in enumerate(film_files):
-            if f_idx % 500 == 0:
-                print("Finished Processing files up to %d" % f_idx)
-            with open(file, 'rb') as f:
-                raw_lines = f.readlines()
-                speaker = None
-                lines = []
-                for line in raw_lines:
-                    # remove stuff in [] or () and strip
-                    line = re.sub("[\(\[].*?[\)\]]", "", line).strip()
-                    if line:
-                        if line.isupper():
-                            speaker = line
-                        elif speaker is not None:
-                            lines.append((speaker, line))
-                            word_cnt += len(line.split())
-                utt_cnt += len(lines)
-                dialogs[file] = lines
-        self.utt_cnt = utt_cnt
-        self.word_cnt = word_cnt
-        self.dialogs = dialogs
-        print("Done parsing all films with %d utts %d words" % (self.utt_cnt, self.word_cnt))
+        if self.dialogs is None:
+            film_files = []
+            for root, _, file_names in os.walk(self._path):
+                for filename in fnmatch.filter(file_names, '*.txt'):
+                    film_files.append(os.path.join(root, filename))
+            print("Loaded %d films" % len(film_files))
+
+            dialogs = {}
+            for f_idx, file in enumerate(film_files):
+                if f_idx % 500 == 0:
+                    print("Finished Processing files up to %d" % f_idx)
+                with open(file, 'rb') as f:
+                    raw_lines = f.readlines()
+                    speaker = None
+                    lines = []
+                    for line in raw_lines:
+                        # remove stuff in [] or () and strip
+                        line = re.sub("[\(\[].*?[\)\]]", "", line).strip()
+                        if line:
+                            if line.isupper():
+                                speaker = line
+                                if "CUT TO" in speaker:
+                                    speaker = None
+                                else:
+                                    speaker = re.sub("[^a-zA-Z0-9 ]+", "", speaker)
+                            elif speaker is not None:
+                                lines.append((speaker, line))
+                    dialogs[file] = self._postprocess_lines(lines)
+            self.dialogs = dialogs
+            self.dump_dialogs()
+
+        self.utt_cnt = np.sum([len(d) for d in self.dialogs.values()])
+        print("Done parsing all films with %d utts" % (self.utt_cnt))
+
+    def _postprocess_lines(self, lines):
+        merge_lines = []
+        prev_speaker = None
+
+        # merge same speaker speech into the same line
+        for cur_speaker, l in lines:
+            if cur_speaker == prev_speaker:
+                merge_lines[-1] = (prev_speaker, merge_lines[-1][1] + " " + l)
+            else:
+                merge_lines.append((cur_speaker, l))
+                prev_speaker = cur_speaker
+
+        # split the same speaker speech into sentences (utterances)
+        split_lines = []
+        for speaker, l in merge_lines:
+            for utt in nltk.sent_tokenize(l):
+                split_lines.append((speaker, utt))
+
+        return split_lines
 
     def pprint_dialog(self, idx):
         lines = self.dialogs.values()[idx]
         for l in lines:
             print("%s: %s" % l)
+
+    def dump_dialogs(self):
+        dest_f = open(self.dump_name, "wb")
+        for file_name, lines in self.dialogs.iteritems():
+            dest_f.write("FILE_NAME: %s\n" % file_name)
+            for speaker, line in lines:
+                dest_f.write("%s|||%s\n" %(speaker, line))
+        dest_f.close()
+
+    def load_dialogs(self):
+        if os.path.exists(self.dump_name):
+            dialogs = {}
+            with open(self.dump_name, "rb") as f:
+                all_lines = f.readlines()
+                file_name = None
+                lines = None
+                for l in all_lines:
+                    l = l.strip()
+                    if "FILE_NAME" in l:
+                        file_name = l.replace("FILE_NAME: ", "")
+                        if lines is not None and len(lines) > 0:
+                            dialogs[file_name] = lines
+                        lines = []
+
+                    elif file_name is not None:
+                        tokens = l.split("|||")
+                        speaker, line = tokens
+                        lines.append((speaker, line))
+            return dialogs
+        else:
+            return None
+
+a = FilmCorpus('Data')
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # DeepLearningProject
 Class Project Repo
+
+clean_data.txt format is as follows:
+FILE_NAME: XXX
+SPEAKER|||TEXT
+
