b7d354a51c82140ee5c80324d096ef00a52ee7bf
snakeztc <zhaotiancheng.hz@gmail.com>
2016-10-26 01:29:13 -0400

    some format retouch

--- data_utils/split_data.py
+++ data_utils/split_data.py
@@ -37,13 +37,14 @@ class data_splitter(object):
         valid_y=open('valid_y.txt','w')
         test_x=open('test_x.txt','w')
         test_y=open('test_y.txt','w')
+
         for idx, line in enumerate(lines):
             if 'FILE_NAME' in line:
                 speaker.append("$$$")
                 utterance.append("$$$")
             else:
-                line=line.replace('\n','')
-                speaker_utterance=line.split("|||")
+                line = line.strip()
+                speaker_utterance = line.split("|||")
                 utterance.append(speaker_utterance[1])
                 speaker.append(speaker_utterance[0])
 
@@ -55,69 +56,72 @@ class data_splitter(object):
         train_char=[]
         test_char=[]
         valid_char=[]
+
         #Pointer for decoder input
         ptr=0
-        while  ptr+1<total_size:
-                Content_x = ""
-                Content_y = ""
-                encoder_speaker = ""
-                decoder_speaker = ""
-                if(speaker[ptr]=="$$$"):
-                    ptr +=1
-                    for i in range(0,line_thres):
-                        Content_x+= ' '+utterance[i+ptr]
-                    ptr += line_thres
-                    encoder_speaker=speaker[ptr-1]
-                    decoder_speaker=speaker[ptr]
-                    if encoder_speaker==decoder_speaker:
-                        Content_x+=' #'
-                    Content_y=utterance[ptr]
+        while ptr+1<total_size:
+            content_x = ""
+            content_y = ""
+            encoder_speaker = ""
+            decoder_speaker = ""
 
-                    if(ptr<=train_size):
-                        train_x.write("$$$\n")
-                        train_y.write("$$$\n")
-                        train_x.write(Content_x+'\n')
-                        train_y.write(Content_y+'\n')
-                        train_char.append(len(Content_x.split(' ')))
+            if (speaker[ptr]=="$$$"):
+                ptr +=1
+                for i in range(0,line_thres):
+                    content_x += ' '+utterance[i+ptr]
+                ptr += line_thres
+                encoder_speaker=speaker[ptr-1]
+                decoder_speaker=speaker[ptr]
+                if encoder_speaker == decoder_speaker:
+                    content_x += ' #'
 
-                    elif ptr>train_size and ptr<train_size+valid_size:
-                        valid_x.write("$$$\n")
-                        valid_y.write("$$$\n")
-                        valid_x.write(Content_x+'\n')
-                        valid_y.write(Content_y+'\n')
-                        valid_char.append(len(Content_x.split(' ')))
-                    else :
-                        test_x.write("$$$\n")
-                        test_y.write("$$$\n")
-                        test_x.write(Content_x+'\n')
-                        test_y.write(Content_y+'\n')
-                        test_char.append(len(Content_x.split(' ')))
+                content_y=utterance[ptr]
 
+                if(ptr<=train_size):
+                    train_x.write("$$$\n")
+                    train_y.write("$$$\n")
+                    train_x.write(content_x+'\n')
+                    train_y.write(content_y+'\n')
+                    train_char.append(len(content_x.split(' ')))
 
+                elif ptr>train_size and ptr<train_size+valid_size:
+                    valid_x.write("$$$\n")
+                    valid_y.write("$$$\n")
+                    valid_x.write(content_x+'\n')
+                    valid_y.write(content_y+'\n')
+                    valid_char.append(len(content_x.split(' ')))
+                else :
+                    test_x.write("$$$\n")
+                    test_y.write("$$$\n")
+                    test_x.write(content_x+'\n')
+                    test_y.write(content_y+'\n')
+                    test_char.append(len(content_x.split(' ')))
+
+            else:
+                start = ptr-line_thres+1
+                for i in range(0, line_thres):
+                    content_x += ' '+utterance[i + start]
+
+                ptr += 1
+                encoder_speaker = speaker[ptr - 1]
+                decoder_speaker = speaker[ptr]
+                if encoder_speaker == decoder_speaker:
+                    content_x+=' #'
+                content_y = utterance[ptr]
+
+                if ptr <= train_size:
+                    train_x.write(content_x + '\n')
+                    train_y.write(content_y + '\n')
+                    train_char.append(len(content_x.split(' ')))
+
+                elif ptr > train_size and ptr < train_size + valid_size:
+                    valid_x.write(content_x + '\n')
+                    valid_y.write(content_y + '\n')
+                    valid_char.append(len(content_x.split(' ')))
                 else:
-                     start=ptr-line_thres+1
-                     for i in range(0, line_thres ):
-                         Content_x += ' '+utterance[i + start]
-                     ptr += 1
-                     encoder_speaker = speaker[ptr - 1]
-                     decoder_speaker = speaker[ptr]
-                     if encoder_speaker==decoder_speaker:
-                         Content_x+=' #'
-                     Content_y = utterance[ptr]
-
-                     if (ptr <= train_size):
-                         train_x.write(Content_x + '\n')
-                         train_y.write(Content_y + '\n')
-                         train_char.append(len(Content_x.split(' ')))
-
-                     elif ptr > train_size and ptr < train_size + valid_size:
-                         valid_x.write(Content_x + '\n')
-                         valid_y.write(Content_y + '\n')
-                         valid_char.append(len(Content_x.split(' ')))
-                     else:
-                         test_x.write(Content_x + '\n')
-                         test_y.write(Content_y + '\n')
-                         test_char.append(len(Content_x.split(' ')))
+                    test_x.write(content_x + '\n')
+                    test_y.write(content_y + '\n')
+                    test_char.append(len(content_x.split(' ')))
 
         print ('Average size of characters in training encoder sentence ' + str(sum(train_char)/train_size))
         print ('Average size of characters in validation encoder sentence ' + str(sum(train_char) / valid_size))
@@ -125,7 +129,6 @@ class data_splitter(object):
 
 
 def main ():
-
     data_dir='/home/ranzhao1/PycharmProjects/DeepLearningProject/data/'
     data_name='filmdata.txt'
     line_thres=2
